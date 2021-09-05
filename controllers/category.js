0eb52538bb91aeb02978729358e7d4c28bc47a28
Navneet-Singh-123 <snavneet561@gmail.com>
2021-09-06 00:40:48 +0530

    CRUD for categories completed

--- controllers/category.js
+++ controllers/category.js
@@ -64,61 +64,6 @@ exports.create = (req, res) => {
   });
 };
 
-// exports.create = (req, res) => {
-//   let form = new formidable.IncomingForm();
-//   form.parse(req, (err, fields, files) => {
-//     if (err) {
-//       return res.status(400).json({
-//         error: "Image upload failed",
-//       });
-//     }
-//     const { name, content } = fields;
-//     const { image } = files;
-//     const slug = slugify(name);
-//     let category = new Category({
-//       name,
-//       content,
-//       slug,
-//     });
-//     if (image.size > 2000000) {
-//       return res.status(400).json({
-//         error: "Image should be less than 2MB",
-//       });
-//     }
-//     // Upload to s3
-//     const params = {
-//       Bucket: "hybrid-tech",
-//       Key: `category/${uuidv4()}`,
-//       Body: fs.readFileSync(image.path),
-//       ACL: "public-read",
-//       ContentType: "image/jpg",
-//     };
-
-//     s3.upload(params, async (err, data) => {
-//       if (err) {
-//         console.log(err);
-//         return res.status(400).json({
-//           error: "Upload to s3 failed",
-//         });
-//       }
-//       console.log("AWS data: ", data);
-//       category.image.url = data.Location;
-//       category.image.key = data.Key;
-
-//       // Save to db
-//       try {
-//         const savedCategory = await category.save();
-//         return res.json(savedCategory);
-//       } catch (error) {
-//         console.log(error);
-//         return res.status(400).json({
-//           error: "Error saving image to the database, category already exists",
-//         });
-//       }
-//     });
-//   });
-// };
-
 exports.list = async (req, res) => {
   try {
     const categories = await Category.find({});
@@ -135,7 +80,7 @@ exports.read = async (req, res) => {
   let { limit, skip } = req.query;
   console.log("Limit: ", limit, "Skip: ", skip);
   limit = limit ? parseInt(limit) : 3;
-  skip = skip ? parseInt(skip) : 0; 
+  skip = skip ? parseInt(skip) : 0;
 
   let category, links;
   try {
@@ -163,6 +108,101 @@ exports.read = async (req, res) => {
   return res.json({ category, links });
 };
 
-exports.update = (req, res) => {};
+exports.update = async (req, res) => {
+  const { slug } = req.params;
+  const { name, image, content } = req.body;
+  const base64Data = new Buffer.from(
+    image.replace(/^data:image\/\w+;base64,/, ""),
+    "base64"
+  );
+  const type = image.split(";")[0].split("/")[1];
+  let updated;
+  try {
+    updated = await Category.findOneAndUpdate(
+      { slug },
+      { name, content },
+      { new: true }
+    );
+  } catch (error) {
+    return res.status(400).json({
+      error: "Could not find the category to update",
+    });
+  }
+  console.log("Updated: ", updated);
+  res.json(updated);
+  //   if (image) {
+  //     // remove the existing from s3 before uploading the updated one
+  //     const deleteParams = {
+  //       Bucket: "hybrid-tech",
+  //       Key: `${updated.image.key}`,
+  //     };
+  //     s3.deleteObject(deleteParams, (err, data) => {
+  //       if (err) {
+  //         console.log("s3 delete error during update: ", err);
+  //       } else {
+  //         console.log("s3 deleted during update", data);
+  //       }
+  //     });
+  //     // uploading the new image
+  //     const params = {
+  //       Bucket: "hybrid-tech",
+  //       Key: `category/${uuidv4()}.${type}`,
+  //       Body: base64Data,
+  //       ACL: "public-read",
+  //       ContentType: `image/${type}`,
+  //       ContentEncoding: "base64",
+  //     };
+  //     s3.upload(params, async (err, data) => {
+  //       if (err) {
+  //         console.log(err);
+  //         return res.status(400).json({
+  //           error: "Upload to s3 failed",
+  //         });
+  //       }
+  //       console.log("AWS data: ", data);
+  //       updated.image.url = data.Location;
+  //       updated.image.key = data.Key;
+  //       updated.postedBy = req.user._id;
 
-exports.remove = (req, res) => {};
+  //       // Save to db
+  //       try {
+  //         const savedCategory = await updated.save();
+  //         return res.json(savedCategory);
+  //       } catch (error) {
+  //         console.log(error);
+  //         return res.status(400).json({
+  //           error: "Error saving image to the database, category already exists",
+  //         });
+  //       }
+  //     });
+  //   } else {
+
+  //   }
+};
+
+exports.remove = async (req, res) => {
+  const { slug } = req.params;
+  try {
+    console.log(slug);
+    const deleted = await Category.findOneAndRemove({ slug });
+    console.log(deleted);
+    const deleteParams = {
+      Bucket: "hybrid-tech",
+      Key: `${deleted.image.key}`,
+    };
+    s3.deleteObject(deleteParams, (err, data) => {
+      if (err) {
+        console.log("s3 delete error: ", err);
+      } else {
+        console.log("s3 deleted", data);
+      }
+    });
+    res.json({
+      message: "Category deleted successfully",
+    });
+  } catch (error) {
+    return res.status(400).json({
+      error: "Could not delete category",
+    });
+  }
+};
