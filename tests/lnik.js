30d2712b6ac9fb9533bbb01631dc8f6fe4bee658
Navneet-Singh-123 <snavneet561@gmail.com>
2022-02-03 02:51:51 +0530

    Testing setup

---
+++ tests/lnik.js
@@ -0,0 +1,9 @@
+const chai = require("chai");
+const expect = chai.expect;
+
+describe("Link Tests", () => {
+  it("Should compare some values", () => {
+    expect(1).to.equal(1);
+  });
+  it("Should do something");
+});
---
+++ tests/user.js
@@ -0,0 +1,6 @@
+const chai = require("chai");
+const expect = chai.expect;
+
+describe("User Tests", () => {
+  it("Should do user tests");
+});
